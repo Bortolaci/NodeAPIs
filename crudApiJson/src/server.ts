071d9dc5630aa8e99b49bbbb94c9dc8706aea766
Bortolaci <joaopedrobortolaci2003@gmail.com>
2023-07-19 21:30:24 -0300

    routes added server running

--- crudApiJson/src/server.ts
+++ crudApiJson/src/server.ts
@@ -1,8 +1,8 @@
-import app from './app';
+import router from './routes/userRouter';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 //Iniciar servidor
-app.listen(PORT, () => console.log(`listening at port: ${PORT}`));
+router.listen(PORT, () => console.log(`listening at port: ${PORT}`));
