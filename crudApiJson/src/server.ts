f288592c7498201d0699e825fe2781562b6cacb4
Bortolaci <joaopedrobortolaci2003@gmail.com>
2023-07-27 20:47:13 -0300

    server running JSON
    Signed-off-by: Bortolaci <joaopedrobortolaci2003@gmail.com>

--- crudApiJson/src/server.ts
+++ crudApiJson/src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //Iniciar servidor
 app.listen(PORT, () => console.log(`listening at port: ${PORT}`));
